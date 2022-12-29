200d95fee6592c1e42d6e02fbcccd10a7c67de35
Memiez <iam_minny@hotmail.com>
2022-12-29 17:00:06 +0700

    Emits

--- src/views/StarredView.vue
+++ src/views/StarredView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import FrameworkPop from "@/components/FrameworkPop.vue";
-const frameList = [
+import { ref, computed } from "vue";
+const frameList = ref([
   {
     name: "VueJS",
     img: "https://img.icons8.com/color/512/vue-js.png",
@@ -16,10 +17,14 @@ const frameList = [
     img: "https://img.icons8.com/color/512/react-native.png",
     rating: 15,
   },
-];
+]);
 function changeRating(index: number, rating: number) {
   console.log("action" + index + " :" + rating);
+  frameList.value[index].rating = rating;
 }
+const orderList = computed(() => {
+  return [...frameList.value].sort((n1, n2) => n2.rating - n1.rating);
+});
 </script>
 
 <template>
@@ -33,6 +38,26 @@ function changeRating(index: number, rating: number) {
           :index="index"
           @change="changeRating"
         ></FrameworkPop>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-table>
+          <thead>
+            <tr>
+              <th>Order</th>
+              <th>Name</th>
+              <th>Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, index) of orderList" :key="index">
+              <td>{{ index + 1 }}</td>
+              <td>{{ item.name }}</td>
+              <td>{{ item.rating }}</td>
+            </tr>
+          </tbody>
+        </v-table>
       </v-col>
     </v-row>
   </v-container>
